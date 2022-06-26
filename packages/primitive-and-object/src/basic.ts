430c6d5c12b4ce2082becb175fa436aa6c7ce660
Madman804 <alvin.wang1202@gmail.com>
2022-06-26 21:35:55 +0200

    chore: commit basic

---
+++ packages/primitive-and-object/src/basic.ts
@@ -0,0 +1,9 @@
+const names: string = 'linbudu';
+const age: number = 24;
+const male: boolean = false;
+const undef: undefined = undefined;
+const nul: null = null;
+const obj: object = { name, age, male };
+const bigintVar1: bigint = 9007199254740991n;
+const bigintVar2: bigint = BigInt(9007199254740991);
+const symbolVar: symbol = Symbol('unique');
