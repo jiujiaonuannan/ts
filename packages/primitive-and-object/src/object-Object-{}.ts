2190630855173e55a3a1198b90a08de58730a4ad
Madman804 <alvin.wang1202@gmail.com>
2022-06-27 23:53:04 +0200

    chore: commit object

---
+++ packages/primitive-and-object/src/object-Object-{}.ts
@@ -0,0 +1,11 @@
+// X
+const tmp25: {} = undefined;
+// X
+const tmp26: {} = null;
+// X
+const tmp27: {} = void 0;
+const tmp28: {} = 'linbudu';
+const tmp29: {} = 599;
+const tmp30: {} = { name: 'linbudu' };
+const tmp31: {} = () => {};
+const tmp32: {} = [];
